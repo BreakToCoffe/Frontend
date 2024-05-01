6e0b5299cb2bdca0e1fb94704922c0fde4b8b42d
4ntonio19 <vgoncalvescarvalho@outlook.com>
2024-04-30 23:06:52 -0300

    inital escope

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  const casa = "Minha casa";
+
+  return (
+    <>
+      <h1 className="titulo">{casa}</h1>
+      <button></button>
+    </>
+  );
+}
+
+export default App;
